3a7e077f75ebf76996d6e6d28e85e04ae59a4630
André Augusto <andreaugustoqueiroz999@gmail.com>
2022-04-21 19:52:47 -0300

    ✨ feat: add select all shortcut: CTRL-A
    🚨 lint(stylua): format with stylua

--- lua/friendly-shortcuts/init.lua
+++ lua/friendly-shortcuts/init.lua
@@ -6,20 +6,20 @@ map('v', '<BS>', 'd')
 
 -- Cut/Copy/Paste
 if vim.fn.has('clipboard') == 1 then
-    -- Cut | CTRL-X and SHIFT-Del
-    map('v', '<C-X>', '+x')
-    map('v', '<S-Del>', '+x')
+  -- Cut | CTRL-X and SHIFT-Del
+  map('v', '<C-X>', '+x')
+  map('v', '<S-Del>', '+x')
 
-    -- Copy | CTRL-C and CTRL-Insert
-    map('v', '<C-C>', '+y')
-    map('v', '<C-Insert>', '+y')
+  -- Copy | CTRL-C and CTRL-Insert
+  map('v', '<C-C>', '+y')
+  map('v', '<C-Insert>', '+y')
 
-    -- Paste | CTRL-V and SHIFT-Insert
-    map('n', 'C-V', 'gP')
-    map('n', 'S-Insert', 'gP')
-    -- Paste in COMMAND mode
-    map('c', 'C-V', '<C-R>+')
-    map('c', 'S-Insert', '<C-R>+')
+  -- Paste | CTRL-V and SHIFT-Insert
+  map('n', 'C-V', 'gP')
+  map('n', 'S-Insert', 'gP')
+  -- Paste in COMMAND mode
+  map('c', 'C-V', '<C-R>+')
+  map('c', 'S-Insert', '<C-R>+')
 end
 
 -- Use CTRL-Q to do what CTRL-V used to do
@@ -37,3 +37,11 @@ map('i', '<C-Z>', '<C-0>u')
 -- Redo | CTRL-Y (except in cmdline, although not repeat)
 map('', '<C-Y>', '<C-R>')
 map('i', '<C-Y>', '<C-O><C-R>')
+
+-- Select all | CTRL-A
+map('', '<C-A>', 'gggH<C-O>G')
+map('i', '<C-A>', '<C-O>gg<C-O>gH<C-O>G')
+map('c', '<C-A>', '<C-C>gggH<C-O>G')
+map('o', '<C-A>', '<C-C>gggH<C-O>G')
+map('s', '<C-A>', '<C-C>gggH<C-O>G')
+map('x', '<C-A>', '<C-C>ggVG')

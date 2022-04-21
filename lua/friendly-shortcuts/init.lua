e750592e4187ec49e9b17af6d9d508f01ba38311
André Augusto <andreaugustoqueiroz999@gmail.com>
2022-04-21 17:40:17 -0300

    ✨ feat: add cut/copy/paste shortcuts: CTRL-X|SHIFT-Del, CTRL-C|CTRL-Ins, CTRL-V|SHIFT-Ins

--- lua/friendly-shortcuts/init.lua
+++ lua/friendly-shortcuts/init.lua
@@ -4,3 +4,20 @@ local map = vim.keymap.set
 -- BACKSPACE in VISUAL mode deletes selection
 map('v', '<BS>', 'd')
 
+-- Cut/Copy/Paste
+if vim.fn.has('clipboard') == 1 then
+    -- Cut | CTRL-X and SHIFT-Del
+    map('v', '<C-X>', '+x')
+    map('v', '<S-Del>', '+x')
+
+    -- Copy | CTRL-C and CTRL-Insert
+    map('v', '<C-C>', '+y')
+    map('v', '<C-Insert>', '+y')
+
+    -- Paste | CTRL-V and SHIFT-Insert
+    map('n', 'C-V', 'gP')
+    map('n', 'S-Insert', 'gP')
+    -- Paste in COMMAND mode
+    map('c', 'C-V', '<C-R>+')
+    map('c', 'S-Insert', '<C-R>+')
+end
